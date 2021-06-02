68c3574bba80d713001c8f4f11978478401d01ef
thang <ben_thang@163.com>
2021-06-02 18:50:17 +0800

    1.1

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const xlsx = require('node-xlsx');
 const nodeExcel = require('excel-export');
+const config = require('./config');
 
 Date.prototype.Format = function (fmt) {
     var o = {
@@ -18,20 +19,29 @@ Date.prototype.Format = function (fmt) {
     return fmt;
   };
 
-const test = JSON.parse(JSON.stringify(require('./test.json')));
+const test = JSON.parse(JSON.stringify(require(`./${config.config.输入数据文件名}.json`)));
+const Jsondata = test.data[config.config.生成表格数据类型];
+let Keys = Object.keys(Jsondata.y_axis[0]);
 
-let testArray = new Array();
-for (let i = 0; i < test.data[100].x_axis.length; i++) {
-    let list = [];
-    list.push(new Date(1000 * test.data[100].x_axis[i]).Format("yyyy-MM-dd hh:mm:ss"));
-    list.push(test.data[100].y_axis[i].value);
-    list.push((test.data[100].y_axis[i].ratio * 100).toFixed(3)+'%');
-    testArray.push(list);
-}
-console.log(testArray);
-// 导出Excel
+
+const xData = Jsondata.x_axis.map((item, i) => 
+     new Date(1000 * item).Format("yyyy-MM-dd hh:mm:ss")
+);
+const yData = Jsondata.y_axis.map((item, i) => {
+
+    if(Keys.includes('ratio')){
+        item.ratio = (item.ratio * 100).toFixed(3)+'%';
+    }
+});
+
+console.log(yData);
+const testArray = Jsondata.x_axis.map((item, i) => 
+        [new Date(1000 * item).Format("yyyy-MM-dd hh:mm:ss"),Jsondata.y_axis[i].value,(Jsondata.y_axis[i].ratio * 100).toFixed(3)+'%']
+)
+
+
 let conf = {} // excel配置
-conf.name = 'sheet' //表格名
+conf.name = config.config.工作表名称; 
 // 列名和类型
 conf.cols = [
     {
@@ -50,7 +60,7 @@ conf.cols = [
 
 conf.rows = testArray;
 let result = nodeExcel.execute(conf)
-let path = `${__dirname}/输出文件.xlsx`
+let path = `${__dirname}/生成文件/${config.config.生成表格文件名}.xlsx`
 fs.writeFile(path, result, 'binary', (err) => {
     err ? console.log(err) : null
 })
